1a0b51441e2e226712f7afaf2a5fc181db37b508
Piotr Bialek <piotrr.b@gmail.com>
2017-03-09 20:54:43 +0100

    Use templateUrl instead of template.
    Add karma-ng-html2js-preprocessor plugin.

--- app/helloWorld/helloWorld.js
+++ app/helloWorld/helloWorld.js
@@ -7,8 +7,6 @@ angular.module("myApp.helloWorld", [])
     return {
         restrict: "AE",
         scope: {name: "=name"},
-        template: "<h1>Hello {{ name.first }} {{ name.last }}!</h1>" +
-            "<input data-ng-model='name.first'></input>" +
-            "<input data-ng-model='name.last'></input>"
+        templateUrl: "helloWorld/helloWorld.html"
     };
 });
--- conf/karma.conf.js
+++ conf/karma.conf.js
@@ -1,17 +1,25 @@
 module.exports = function(config) {
     config.set({
+        preprocessors: {
+            'app/**/*.html': ['ng-html2js']
+        },
         basePath : '../',
         files : [
             'node_modules/angular/angular.js',
             'node_modules/angular-mocks/angular-mocks.js',
             'app/**/*.js',
-            'test/unit/**/*.js'
+            'test/unit/**/*.js',
+            'app/**/*.html'
         ],
         autoWatch: true,
         frameworks: ['jasmine'],
         browsers : ['Chrome'],
         plugins : [
             'karma-chrome-launcher',
-            'karma-jasmine'
-        ]
+            'karma-jasmine',
+            'karma-ng-html2js-preprocessor'
+        ],
+        ngHtml2JsPreprocessor: {
+            stripPrefix: 'app/'
+        }
 })};
